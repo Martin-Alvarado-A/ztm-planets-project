c3018d07b175ed470e7cb7a1ed53a46555ba9c4f
martin <martin.alvarado2002@gmail.com>
2024-02-05 19:35:20 +0100

    [6-64]: Reading Our Planets Data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(`🔎 | index | error | ERROR:`, err);
+  })
+  .on('end', () => {
+    console.log(`🔎 | index | end | results:`, results);
+    console.log(`🔎 | index | end | DONE`);
+  });
